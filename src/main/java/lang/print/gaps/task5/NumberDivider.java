79e17002a170febccf6862a75f68d0337810e292
i.shirinov <shirinovilyos6965@gmail.com>
2022-12-05 14:35:06 +0500

    Vazifa №5. Operatorlar new

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,13 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int a = 10, b = 5;
-        System.out.println(a / b);
+//        double x;
+//        ()x = toBeDivided / 5;
+        System.out.println((double) toBeDivided / 5);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider divider = new NumberDivider();
+        divider.divide(11);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,12 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        int daqiqa = 60, soniya = 60;
-        System.out.println(daqiqa * soniya);
+
+        System.out.println(minutes * 60);
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor convertor=new TimeConvertor();
+        convertor.convert(10);
     }
 }
